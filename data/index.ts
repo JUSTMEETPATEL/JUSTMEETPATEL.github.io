1e406356a121ed0f86472859f21bc44a52a6e089
JUSTMEETPATEL <justmeetpatel@gmail.com>
2024-06-08 22:32:55 +0530

    minor commit

---
+++ data/index.ts
@@ -0,0 +1,44 @@
+export const projects = [
+    {
+        id: 1,
+        title: "PassInk",
+        des: "PassInk is a Password Manager which saves the website, username and password in device's local storage",
+        img: "/passink.png",
+        iconLists: ["/react.svg", "/tailwindcss.svg", "/javascript.svg"],
+        link: "https://meetpatel.me/PassInk",
+    },
+    {
+        id: 2,
+        title: "Clutter Folder Manager",
+        des: "Clutter Folder Manager is a node.js based program which sort out the files of the given directory as per there file extension",
+        img: "/clutter.jpeg",
+        iconLists: ["/javascript.svg"],
+        link: "https://github.com/JUSTMEETPATEL/ClutterFolderManager",
+    },
+    {
+        id: 3,
+        title: "Valetudo",
+        des: "Valetduo was a python gui program which have login system with otp verification, this also have a BMI calculator which provide a diet plan as per the bmi",
+        img: "/valetudo.png",
+        iconLists: ["/python.svg", "/mysql.svg"],
+        link: "https://github.com/JUSTMEETPATEL/BMI-with-Login-system-in-Python-using-GUI",
+    },
+];
+
+export const socialMedia = [
+    {
+        id: 1,
+        img: "/github.svg",
+        link:"https://www.github.com/JUSTMEETPATEL"
+    },
+    {
+        id: 2,
+        img: "/insta.svg",
+        link:"https://www.instagram.com/just_meet05/"
+    },
+    {
+        id: 3,
+        img: "/linkedin.svg",
+        link:"https://www.linkedin.com/in/meetpatel011/"
+    },
+];
